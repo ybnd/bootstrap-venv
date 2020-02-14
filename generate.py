ab3f28134d0fa896dda27e84e4086311df66557c
ybnd <ybnd@tuta.io>
2020-02-14 12:18:28 +0100

    Clean up a bit

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ from faker import Faker
 
 print(f"\n\nRunning '{__file__}' with {sys.executable}\n")
 
-# Generate some text using a module we hadn't had installed
+# Generate some text using a module we've just installed
 fake = Faker()
 
 with open('content', 'w+') as f:
--- undeploy.py
+++ undeploy.py
@@ -1,9 +1,10 @@
 import os
 import shutil
 
+# Remove virtual environment
 if os.path.isdir('.venv'):
     shutil.rmtree('.venv')
     
-    
+# Remove generated file
 if os.path.isfile('content'):
     os.remove('content')
